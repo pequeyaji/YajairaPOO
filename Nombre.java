5d568167d13125ccef3c60b6edee4a4284616880
yajaira pech <118487544+pequeyaji@users.noreply.github.com>
2023-04-23 15:21:57 -0600

    Add files via upload

---
+++ Nombre.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Nombre {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner Nombre = new Scanner(System.in);
+String nombre, apellido;
+System.out.println("Ingresa tu nombre");
+nombre = Nombre.nextLine();
+System.out.println("Ingresa tu apellido");
+apellido = Nombre.nextLine();
+System.out.println(" Tu nombre es" + nombre + apellido);
+	}
+
+}
